aafd5534ba7d322297a6430051352a98248fac57
= <vini.delimagomes@gmail.com>
2024-11-20 19:33:25 -0300

    git init

---
+++ estoque/src/test/java/com/fiap/spring/cloud/pedidos/EstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.spring.cloud.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ pedidos/src/test/java/com/fiap/spring/cloud/estoque/FiapSpringFase4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.spring.cloud.estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapSpringFase4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
